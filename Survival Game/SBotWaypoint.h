487c317cbcf9e4e585d6ec40e5e22b26259e622a
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2017-11-15 15:45:15 +0000

    Create SBotWaypoint.h
    Waypoint helper for bots to generate waypoints during patrols. Object is placed in level to specify a potential patrol target location.

---
+++ Survival Game/SBotWaypoint.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Engine/TargetPoint.h"
+#include "SBotWaypoint.generated.h"
+
+UCLASS()
+class SURVIVALGAME_API ASBotWaypoint : public ATargetPoint
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
