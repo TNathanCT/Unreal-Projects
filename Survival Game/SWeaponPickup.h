71cb128a5db40953b4f5f26691014bcf882c7e2d
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2017-11-17 11:12:10 +0000

    Create SWeaponPickup.h
    This script adds the weapon to the inventory when it has been picked up.

---
+++ Survival Game/SWeaponPickup.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "SPickupActor.h"
+#include "SWeaponPickup.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Abstract)
+class SURVIVALGAME_API ASWeaponPickup : public ASPickupActor
+{
+	GENERATED_BODY()
+
+	ASWeaponPickup(const FObjectInitializer& ObjectInitializer);
+
+public:
+
+	/* Class to add to inventory when picked up */
+	UPROPERTY(EditDefaultsOnly, Category = "WeaponClass")
+	TSubclassOf<ASWeapon> WeaponClass;
+
+	virtual void OnUsed(APawn* InstigatorPawn) override;
+};
