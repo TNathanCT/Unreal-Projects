33920057a0f194983987c08a3045116f93a8733f
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2017-11-18 21:01:32 +0000

    Create ASPlayerStart.h
    This is the condition of the player when he or she is spawned.

---
+++ Survival Game/ASPlayerStart.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "GameFramework/PlayerStart.h"
+#include "SPlayerStart.generated.h"
+
+UCLASS()
+class SURVIVALGAME_API ASPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+
+	ASPlayerStart(const class FObjectInitializer& ObjectInitializer);
+
+	/* Is only useable by players - automatically a preferred spawn for players */
+	UPROPERTY(EditAnywhere, Category = "PlayerStart")
+	bool bPlayerOnly;
+
+public:
+
+	bool GetIsPlayerOnly() { return bPlayerOnly; }
+
+};
