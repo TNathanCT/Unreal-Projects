b32c3b2953fb519ae76ce399937c0cc91bfef04b
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2017-11-18 20:50:48 +0000

    Create SSpectatorPawn.h
    This script initializes the spawning of pawns.

---
+++ Survival Game/SSpectatorPawn.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "GameFramework/SpectatorPawn.h"
+#include "SSpectatorPawn.generated.h"
+
+UCLASS()
+class SURVIVALGAME_API ASSpectatorPawn : public ASpectatorPawn
+{
+	GENERATED_BODY()
+
+	ASSpectatorPawn(const class FObjectInitializer& ObjectInitializer);
+	
+};
