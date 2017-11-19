42065e304545a49666fb988b3b17245543e18914
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2017-11-19 15:27:40 +0000

    Create SOpenWorldGameMode.h
    This script helps prevent outside players and bots to spectate the game.

---
+++ Survival Game/SOpenWorldGameMode.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "World/SGameMode.h"
+#include "SOpenWorldGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ABSTRACT)
+class SURVIVALGAME_API ASOpenWorldGameMode : public ASGameMode
+{
+	GENERATED_BODY()
+
+	ASOpenWorldGameMode(const FObjectInitializer& ObjectInitializer);
+	
+	/* Don't allow spectating of other players or bots */
+	virtual bool CanSpectate_Implementation(APlayerController* Viewer, APlayerState* ViewTarget) override;
+};
